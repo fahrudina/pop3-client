2154b3fb0f74675307ab8c43e9d723d036b76f55
fahrudin <fahrudin@helio.id>
2018-03-18 22:52:39 +0700

    save UID To database

--- pop3client.go
+++ pop3client.go
@@ -1,21 +1,22 @@
 package pop3client
 
 import (
-	"strings"
-	"github.com/fahrudina/go-pop3"
-	"github.com/helioina/api/log"
-	"net/smtp"
-	"net/mail"
-	"fmt"
 	"bytes"
+	"fmt"
+	"net/mail"
+	"net/smtp"
+	"strings"
 	"time"
+
+	"github.com/fahrudina/go-pop3"
 	"github.com/helioina/api/contexts"
+	"github.com/helioina/api/log"
 )
 
-func InitiateGetMessages(pop3Address, username , password, email string, ctx *contexts.Context){
-	c, err := pop3.Dial( pop3Address , pop3.UseTimeout(0) )
+func InitiateGetMessages(pop3Address, username, password, email string, ctx *contexts.Context) {
+	c, err := pop3.Dial(pop3Address, pop3.UseTimeout(0))
 	//log.LogTrace("%s----- %s ", username, password)
-	if err != nil{
+	if err != nil {
 		log.LogError(err.Error())
 		return
 	}
@@ -23,134 +24,221 @@ func InitiateGetMessages(pop3Address, username , password, email string, ctx *co
 	if err := c.User(username); err != nil {
 		log.LogError(err.Error())
 		c.Quit()
-		return 
+		return
 	}
-	if err := c.Pass(password); err !=nil {
+	if err := c.Pass(password); err != nil {
 		log.LogError(err.Error())
 		c.Quit()
-		return 
+		return
 	}
 
-	count, size, err := c.Stat() 
-	if err !=nil {
+	count, size, err := c.Stat()
+	if err != nil {
 		log.LogError(err.Error())
 		c.Quit()
-		return 
-	}else{
+		return
+	}
+	log.LogInfo("count : %d -- size: %d", count, size)
+
+	msgInfo, err := c.UIDlAll()
+	if err != nil {
+		log.LogError(err.Error())
+		c.Quit()
+		return
+	} else {
 		var download uint32
 		dataPop := map[string]interface{}{
-			"username" : username,
-			"email": email,
-			"password": encPasswd(password), //password,
-			"count": count,
-			"downloaded": download,
+			"username":     username,
+			"email":        email,
+			"password":     encPasswd(password), //password,
+			"count":        count,
+			"uidl":         msgInfo,
+			"downloaded":   download,
 			"lastdownload": time.Now(),
 		}
-		if err := ctx.Ds.AddDataPop3Client(dataPop); err != nil{
+		if err := ctx.Ds.AddDataPop3Client(dataPop); err != nil {
 			log.LogError(err.Error())
 		}
 	}
-	log.LogInfo("count : %d -- size: %d", count, size)
 
-	msgInfo, err := c.UIDlAll() 
-	if err !=nil {
+	uidl, err := ctx.Ds.GetPop3UIDL(username)
+	if err != nil {
 		log.LogError(err.Error())
-		c.Quit()
-		return 
-	}
+	} else {
+		log.LogTrace("<%d>", len(uidl.Uidl))
+		countMsg := len(uidl.Uidl)
 
-	for _,v := range msgInfo{
-		if v.Seq != count{ // For testing get last email only
-			msg, err := c.Retr(v.Seq)
-			if err !=nil {
-				log.LogError(err.Error())
-			}else{
-				if err := sendMessages(msg, email); err == nil{
-					fieldsToUpdate:= map[string]interface{}{
-						"msgdownloaded": +1,
-					}
-					if err := ctx.Ds.UpdatePop3Data(username, fieldsToUpdate); err != nil{
+		if len(uidl.Uidl) <= 100 {
+			for _, v := range uidl.Uidl {
+				msg, err := c.Retr(v.Seq)
+				if err != nil {
+					log.LogError(err.Error())
+				} else {
+					if err := sendMessages(msg, email); err != nil {
 						log.LogError(err.Error())
 					}
 				}
-
+			}
+		} else {
+			loop := 1
+			for _, _ = range uidl.Uidl {
+				if loop <= 100 {
+					msg, err := c.Retr(uint32(countMsg))
+					if err != nil {
+						log.LogError(err.Error())
+					} else {
+						if err := sendMessages(msg, email); err != nil {
+							log.LogError(err.Error())
+						}
+					}
+					countMsg--
+				}
+				loop++
 			}
 		}
 	}
 
+	// var loop int
+	// var countMsg uint32
+	// countMsg = count
+	// for _, v := range msgInfo {
+
+	// 	if loop <= 100 && count >= 100 {
+	// 		msg, err := c.Retr(countMsg)
+	// 		if err != nil {
+	// 			log.LogError(err.Error())
+	// 		} else {
+	// 			if err := sendMessages(msg, email); err == nil {
+	// 				fieldsToUpdate := map[string]interface{}{
+	// 					"msgdownloaded": +1,
+	// 				}
+	// 				if err := ctx.Ds.UpdatePop3Data(username, fieldsToUpdate); err != nil {
+	// 					log.LogError(err.Error())
+	// 				}
+	// 			}
+	// 		}
+	// 		loop++
+	// 		countMsg--
+	// 	} else if count <= 100 {
+	// 		msg, err := c.Retr(v.Seq)
+	// 		if err != nil {
+	// 			log.LogError(err.Error())
+	// 		} else {
+	// 			if err := sendMessages(msg, email); err == nil {
+	// 				fieldsToUpdate := map[string]interface{}{
+	// 					"msgdownloaded": +1,
+	// 				}
+	// 				if err := ctx.Ds.UpdatePop3Data(username, fieldsToUpdate); err != nil {
+	// 					log.LogError(err.Error())
+	// 				}
+	// 			}
+
+	// 		}
+	// 	} else {
+	// 		fieldsToUpdate := map[string]interface{}{
+	// 			"msgdownloaded": +1,
+	// 		}
+	// 		if err := ctx.Ds.UpdatePop3Data(username, fieldsToUpdate); err != nil {
+	// 			log.LogError(err.Error())
+	// 		}
+	// 	}
+	//}
+
 	// Send the QUIT command and close the connection.
-	if err := c.Quit(); err !=nil {
+	if err := c.Quit(); err != nil {
 		log.LogError(err.Error())
 	}
 }
 
-func GetMessages(pop3Address, username string, ctx *contexts.Context){
+func GetMessages(pop3Address, username string, ctx *contexts.Context) {
 
-	c, err := pop3.Dial( pop3Address , pop3.UseTimeout(0) )
-	if err != nil{
+	c, err := pop3.Dial(pop3Address, pop3.UseTimeout(0))
+	if err != nil {
 		log.LogError(err.Error())
 		return
 	}
 
-	popData , err := ctx.Ds.CheckPop3Download(username)
-	if err != nil{
+	popData, err := ctx.Ds.CheckPop3Download(username)
+	if err != nil {
 		log.LogError(err.Error())
 		return
 	}
-	
+
 	if err := c.User(popData.Username); err != nil {
 		log.LogError(err.Error())
 		c.Quit()
-		return 
+		return
 	}
-	if err := c.Pass(decPassword( popData.Password)); err !=nil {
+	if err := c.Pass(decPassword(popData.Password)); err != nil {
 		log.LogError(err.Error())
 		c.Quit()
-		return 
+		return
 	}
 
-	count, size, err := c.Stat() 
-	if err !=nil {
+	count, size, err := c.Stat()
+	if err != nil {
 		log.LogError(err.Error())
 		c.Quit()
-		return 
+		return
 	}
 	log.LogInfo("count : %d -- size: %d", count, size)
 
-	msgInfo, err := c.UIDlAll() 
-	if err !=nil {
+	msgInfo, err := c.UIDlAll()
+	if err != nil {
 		log.LogError(err.Error())
 		c.Quit()
-		return 
+		return
 	}
-	amount := popData.MsgDownloaded
-	for _,_ = range msgInfo{
-		if amount != count{ 
-			amount++
-			msg, err := c.Retr(amount)
-			if err !=nil {
-				log.LogError(err.Error())
-			}else{
-				if err := sendMessages(msg, popData.Email); err != nil{
-					log.LogError(err.Error())
-				}else{
-					fieldsToUpdate:= map[string]interface{}{ "msgdownloaded": +1 }
 
-					if err := ctx.Ds.UpdatePop3Data(popData.Username, fieldsToUpdate); err != nil{
+	for _, v := range msgInfo {
+		if ctx.Ds.CheckPop3UID(popData.Username, v.UID) {
+			msg, err := c.Retr(v.Seq)
+			if err != nil {
+				log.LogError(err.Error())
+			} else {
+				if err := sendMessages(msg, popData.Email); err != nil {
+					log.LogError(err.Error())
+				} else {
+					fieldsToUpdate := map[string]interface{}{"uidl.seq": v.Seq, "uidl.size": v.Size, "uidl.uid": v.UID}
+					if err := ctx.Ds.AddtoSePop3Data(popData.Username, fieldsToUpdate); err != nil {
 						log.LogError(err.Error())
 					}
 				}
 			}
 		}
 	}
+	// msgDwnlod := 0
+	// amount := popData.MsgDownloaded
+	// for _, _ = range msgInfo {
+	// 	if amount != count {
+	// 		if amount < count {
+	// 			amount++
+	// 			msgDwnlod = 1
+	// 		}
+	// 		msg, err := c.Retr(amount)
+	// 		if err != nil {
+	// 			log.LogError(err.Error())
+	// 		} else {
+	// 			if err := sendMessages(msg, popData.Email); err != nil {
+	// 				log.LogError(err.Error())
+	// 			} else {
+	// 				fieldsToUpdate := map[string]interface{}{"msgdownloaded": +msgDwnlod}
+
+	// 				if err := ctx.Ds.UpdatePop3Data(popData.Username, fieldsToUpdate); err != nil {
+	// 					log.LogError(err.Error())
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 
 	// Send the QUIT command and close the connection.
-	if err := c.Quit(); err !=nil {
+	if err := c.Quit(); err != nil {
 		log.LogError(err.Error())
 	}
 }
 
-func sendMessages( data , emailRcpt string) error {
+func sendMessages(data, emailRcpt string) error {
 	// Connect to the remote SMTP server.
 	c, err := smtp.Dial("localhost:2525")
 	if err != nil {
@@ -158,17 +246,17 @@ func sendMessages( data , emailRcpt string) error {
 		return err
 	}
 	senderRcpt := getSenderRcpt(data)
-	getSndRcpt := strings.Split(senderRcpt,"~")
+	getSndRcpt := strings.Split(senderRcpt, "~")
 	log.LogTrace(senderRcpt)
 	// Set the sender and recipient first
-	if err := c.Mail(strings.Replace(getSndRcpt[0],"<", "", -1 ) ); err != nil {
-			log.LogError(err.Error())
-			return err
+	if err := c.Mail(strings.Replace(getSndRcpt[0], "<", "", -1)); err != nil {
+		log.LogError(err.Error())
+		return err
 	}
 
-	if err := c.Rcpt(emailRcpt/*getSndRcpt[1]*/); err != nil {
-			log.LogError(err.Error())
-			return err
+	if err := c.Rcpt(emailRcpt /*getSndRcpt[1]*/); err != nil {
+		log.LogError(err.Error())
+		return err
 	}
 
 	// Send the email body.
@@ -197,49 +285,55 @@ func sendMessages( data , emailRcpt string) error {
 	return nil
 }
 
-func getSenderRcpt(msg string)string{
+func getSenderRcpt(msg string) string {
 	if rm, err := mail.ReadMessage(bytes.NewBufferString(msg)); err == nil {
 		mailFrom := senderRcptParser(rm.Header.Get("From"))
 		mailTo := senderRcptParser(rm.Header.Get("To"))
 		mailCc := senderRcptParser(rm.Header.Get("Cc"))
-		return fmt.Sprintf("%s~%s~%s", mailFrom, mailTo , mailCc )
-	}else{
+		return fmt.Sprintf("%s~%s~%s", mailFrom, mailTo, mailCc)
+	} else {
 		log.LogError(err.Error())
 	}
-	
+
 	return ""
 }
 
-func senderRcptParser( mailAccount string ) string {
+func senderRcptParser(mailAccount string) string {
 	var accParser string
-	if strings.Contains(mailAccount, "<"){
-		mAccSpl := strings.Split(mailAccount,"<")
+	if strings.Contains(mailAccount, "<") {
+		mAccSpl := strings.Split(mailAccount, "<")
 		accParser = strings.TrimLeft(strings.TrimRight(mAccSpl[1], ">"), "<")
-	}else{
+	} else {
 		accParser = strings.TrimLeft(strings.TrimRight(mailAccount, ">"), "<")
 	}
 	return accParser
 }
 
-func encPasswd(password string)[]byte {
-    key := []byte("67890987654321234567890098765432") // 32 bytes
-    plaintext := []byte(password)
-    ciphertext, err := encrypt(key, plaintext)
-    if err != nil {
-        log.LogError(err.Error())
+func encPasswd(password string) []byte {
+	key := []byte("Mtiertt44LFatieh1238LsdbaLPsakmV") // 32 bytes
+	plaintext := []byte(password)
+	ciphertext, err := encrypt(key, plaintext)
+	if err != nil {
+		log.LogError(err.Error())
 	}
 	return ciphertext
 }
 
-func decPassword(ciphertext []byte)string{
-	key := []byte("67890987654321234567890098765432") // 32 bytes
+func decPassword(ciphertext []byte) string {
+	key := []byte("Mtiertt44LFatieh1238LsdbaLPsakmV") // 32 bytes
 	result, err := decrypt(key, ciphertext)
-    if err != nil {
+	if err != nil {
 		log.LogError(err.Error())
-    }
-    return bytesToString(result)
+	}
+	return bytesToString(result)
 }
 
 func bytesToString(data []byte) string {
 	return string(data[:])
+}
+
+func UpdateCredential(username, password string, ctx *contexts.Context) {
+	if err := ctx.Ds.UpdatePop3Credential(username, encPasswd(password)); err != nil {
+		log.LogError(err.Error())
+	}
 }
